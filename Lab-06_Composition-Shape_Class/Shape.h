fd49f8425480c25904917f7c49e926d0d9f131f0
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-09-23 14:43:27 -0400

    Adding first lines of code for the Shape.h class

---
+++ Lab-06_Composition-Shape_Class/Shape.h
@@ -0,0 +1,28 @@
+//
+// Lab-06 Shapes
+// Composition in Class creation
+// Alberto A. Colón Reyes ID 67689
+// CECS 2223 - Prof. Carlos De La Torre Ugarte
+// 
+
+#ifndef _Shape_h
+#define _Shape_h
+
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+class Shape
+{
+    private:
+    Point points[6];
+    int howManyPoints;
+
+    public:
+    
+
+};
+
+#endif
+// _Shape
