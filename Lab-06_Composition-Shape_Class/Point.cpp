624fa5bb29f36b6b2215f11154f6d0fb6ff28f71
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-09-23 10:54:21 -0400

    Complete the Definitions file for the Point class

--- Lab-06_Composition-Shape_Class/Point.cpp
+++ Lab-06_Composition-Shape_Class/Point.cpp
@@ -5,3 +5,68 @@
 // CECS 2223 - Prof. Carlos De La Torre Ugarte
 // 
 
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+// Point Class methods definitions
+
+Point::Point(int x, int)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Point::Point(const Point & aPoint)
+{
+    (*this) = aPoint;
+}
+
+Point::~Point()
+{
+    // None resourse to freed.
+}
+
+Point & Point::operator = (const Point & aPoint)
+{
+    this->x = aPoint.x;
+    this->y = aPoint.y;
+    return(*this);
+}
+    
+int Point::getX() const
+{
+    return(this->x);
+}
+
+int Point::getY() const
+{
+    return(this->y);
+}
+
+void Point::setX(int x)
+{
+    this->x = x;
+}
+
+void Point::setY(int y)
+{
+    this->y = y;
+}
+
+ostream & operator << (ostream & out, const Point & aPoint)
+{
+    out << "(" << aPoint.x << " , " << aPoint.y << ")";
+    return(out);
+}
+
+istream & operator >> (istream & in, Point & aPoint)
+{
+    cout << "Add the X coordenate of the point: ";
+    in >> aPoint.x;
+    cout << "Add the Y coordenate of the point: ";
+    in >> aPoint.y;
+
+    return(in);
+}
