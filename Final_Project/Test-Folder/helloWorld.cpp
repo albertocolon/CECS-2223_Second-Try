5cd7e4f3a4b294d39267ad1fb3415ffbbe7612ca
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-10-11 14:43:23 -0400

    Just adding a file to test git

---
+++ Final_Project/Test-Folder/helloWorld.cpp
@@ -0,0 +1,18 @@
+// Test program to save on GitHub
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    string a = "Hello";
+    string b = "World";
+
+    cout << "Hey Everybody!" << endl;
+    cout << a << " " << b << endl;
+
+    return 0; 
+}
