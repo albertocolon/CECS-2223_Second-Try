52f2d958be068d8e89f04985c28db7dab5451231
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-10-21 22:25:25 -0400

    Adding more files and code

---
+++ Final_Project/House-Realty/House.h
@@ -0,0 +1,27 @@
+//
+//  House.h
+//  Final-Project Lab-HomeRealty
+//
+//  Created by Alberto Colon on 10/21/23.
+//
+
+#ifndef House_h
+#define House_h
+
+#include "MyString.h"
+#include "MyDate.h"
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+class House
+{
+    private:
+    MyString description;
+
+
+};
+
+#endif /* House_h */
---
+++ Final_Project/House-Realty/Properties.h
@@ -0,0 +1,32 @@
+//
+//
+//  House.h
+//  Final-Project Lab-HomeRealty
+//
+//  Created by Alberto Colon on 10/21/23.
+//
+
+#ifndef Properties_h
+#define Properties_h
+
+#include "MyString.h"
+#include "MyDate.h"
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+class Properties
+{
+    private:
+    
+};
+
+
+
+
+
+
+
+#endif /* Properties_h */
