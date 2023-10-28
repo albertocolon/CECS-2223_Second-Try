81c14546364afdcdfc0ef67516ec1c6f464af3f8
Alberto A. Colón Reyes <alberto_colon@me.com>
2023-10-28 18:12:37 -0400

    Create the header for the house class

--- Final_Project/House-Realty/House.h
+++ Final_Project/House-Realty/House.h
@@ -9,6 +9,9 @@
 #define House_h
 
 #include "MyString.h"
+#include "MyInt.h"
+#include "MyStringArray.h"
+#include "Properties.h"
 #include "MyDate.h"
 #include <stdio.h>
 #include <cstring>
@@ -16,11 +19,45 @@
 
 using namespace std;
 
-class House
+class House: public Properties
 {
     private:
-    MyString description;
+    MyInt ID;
+    double price;
+    MyDate addDate;
 
+    public:
+    House(const MyInt &anID = 0, const double &aPrice = 0.0, const MyDate &aDate = MyDate());
+    House(const House &aHouse);
+    virtual ~House();
+
+    House &operator=(const House &aHouse);
+
+    void setID(const MyInt &anID);
+    void setID(const int &anID);
+    void setPrice(const double &aPrice);
+    void setAddDate(const MyDate &aDate);
+    void setAddDate(const int &aMonth, const int &aDay, const int &aYear);
+
+
+    const MyInt &getID()const;
+    const double &getPrice()const;
+    const MyDate &getAddDate()const;
+
+    virtual void print()const;
+    virtual void input();
+    virtual void printToFile(ofstream &out)const;
+
+    friend ostream &operator<<(ostream &out, const House &aHouse);
+    friend istream &operator>>(istream &in, House &aHouse);
+    
+    friend ofstream &operator<<(ofstream &out, const House &aHouse);
+    friend ifstream &operator>>(ifstream &in, House &aHouse);
+
+    friend bool operator==(const House &aHouse, const House &bHouse);
+    friend bool operator!=(const House &aHouse, const House &bHouse);
+
+    virtual MyString getType()const;
 
 };
 
